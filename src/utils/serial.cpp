6fba976e4720945684baea5d1e67beab22180998
Ben Beaudet <bbeaudet0@gmail.com>
2025-06-24 16:18:33 -0400

    Update serial.cpp

--- src/utils/serial.cpp
+++ src/utils/serial.cpp
@@ -5,15 +5,15 @@
 
 Serial *Serial::create (const char *port_name, Board *board)
 {
-// #ifdef USE_LIBFTDI
-//     {
-//         LibFTDISerial ftdi_probe (port_name);
-//         if (ftdi_probe.open_serial_port () != SerialExitCodes::PORT_NAME_ERROR)
-//         {
-//             return new LibFTDISerial (port_name, board);
-//         }
-//     }
-// #endif
+#ifdef USE_LIBFTDI
+    {
+        LibFTDISerial ftdi_probe (port_name);
+        if (ftdi_probe.open_serial_port () != SerialExitCodes::PORT_NAME_ERROR)
+        {
+            return new LibFTDISerial (port_name, board);
+        }
+    }
+#endif
 
     return new OSSerial (port_name);
 }
